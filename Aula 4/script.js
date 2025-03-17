8666deed9bef04061a8dd56c770a5ada7efe3e5f
novakleal <167883141+novakleal@users.noreply.github.com>
2025-03-17 11:16:27 -0300

    Update script.js

--- Aula 4/script.js
+++ Aula 4/script.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-
+    const diminuirFonteBotao = document.getElementById('diminuir-fonte');
     let tamanhoAtualFonte = 1;
 
     aumentaFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte += 0.1;
+        tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
     })
